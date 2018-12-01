53cb1a747ca1d70558f11b545ecbb1f374dc4623
GuilhermeVieira <xgvieira@gmail.com>
2018-12-01 21:25:03 -0200

    Add report and graphics

--- problem.py
+++ problem.py
@@ -1,31 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-from scipy.optimize import linprog
 from simplex import simplex
 
-def plotDiscretization(A, dx, title, xlabel, ylabel):
-    x = 0
+def plot_discretization(A, dx, title, xlabel, ylabel):
     # Create figure and axes
     fig, ax = plt.subplots(1)
-    plt.ylim([-dx, dx])
+    if title == "Position Graph":
+        plt.ylim([-2, 2])
+    else:
+        plt.ylim([-dx, dx])
     plt.xlim([0,10])
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.title(title)
-    for i in range(len(A)):
-        if A[i] >= 0:
-            rect = Rectangle((x,0), dx, A[i],linewidth=1,edgecolor='r',facecolor='none')
-        # Add the patch to the Axes
-        else:
-            rect = Rectangle((x,A[i]), dx, -A[i],linewidth=5,edgecolor='r',facecolor='none')
-        ax.add_patch(rect)
-        x = x + dx
+    x = []
+    x.append(0)
+    for i in range(1, len(A)):
+        x.append(x[i-1] + dx)
+
+    plt.plot(x, A, 'ro')
     plt.show()
 
     return
 
-def printConstraint(c, b, n):
+def print_constraint(c, b, n):
     for i in range(0, 2*n, 2):
         print("+" + str(int(c[i])) + "*a" + str(int(i/2 + 1)) + "^+ "
               + str(int(c[i+1])) + "*a" + str(int(i/2 + 1)) + "^⁻ ", end = '')
@@ -33,29 +32,45 @@ def printConstraint(c, b, n):
 
     return
 
-def printConstraints(A_eq, b_eq, n):
+def print_problem(A_eq, b_eq, c, t, n):
+    print("minimizar")
+
+    for i in range(0, 2*n, 2):
+        print(str(t) + "*a" + str(int(i/2 + 1)) + "^+ "
+              + str(t) + "*a" + str(int(i/2 + 1)) + "^⁻ ", end = '')
+    print("")
+
+    print("Sujeito a:")
     # Prints position constraint
-    printConstraint(A_eq[0], b_eq[0], n)
+    print_constraint(A_eq[0], b_eq[0], n)
     # Prints velocity constraint
-    printConstraint(A_eq[1], b_eq[1], n)
+    print_constraint(A_eq[1], b_eq[1], n)
+    print("Todo ai >= 0")
 
     return
 
-def findAccelerations(x, n):
+def find_accelerations(x, n):
     A = []
     for i in range(0,n, 2):
         A.append(x[i] - x[i+1]) # xi = xi^+ - xi^-
     return A
 
-def main():
+def get_cost(c, sol):
+    cost = 0
+    for i in range(len(c)):
+        cost += c[i]*sol[i]
+
+    return cost
+
+def problem(time_increment, is_single):
     t = 10
-    time_increment = float(input('Enter the discretization interval: '))
     n = int(t/time_increment)
 
     # Constructing the coefficients vector of the objective function
     c = []
     for i in range(2*n):
         c.append(time_increment)
+    c_copy = c.copy()
 
     # Constructing the equality constraints matrix
     A_eq = [[], []]
@@ -72,52 +87,66 @@ def main():
         A_eq[1].append(1)
         A_eq[1].append(-1)
 
-    # Constructing bound variables
-    bounds = ()
-    for i in range (2*n):
-        bounds = bounds + ((0, None), )
-
     # Printing the problem
-    printConstraints(A_eq, b_eq, n)
-    print(bounds)
-    res = linprog(c, A_eq= A_eq, b_eq=b_eq, bounds=bounds, options={"disp": True})
-
-    # Adding the values of a0^+ and a0^-
-    '''
-    x = [0, 0]
-    for i in range(2*n):
-        x.append(res.x[i])
+    if is_single:
+        print_problem(A_eq, b_eq, c, time_increment, n)
 
     # Printing results
     print("Simplex result")
-    '''
-    print(res)
-
-    '''
-    A = findAccelerations(x, 2*n + 2)
-    print("Accelerations")
-    print(A)
-    '''
-    # Second simplex
     sol = simplex(c, A_eq, b_eq)
     print(sol)
-    '''
-    plotDiscretization(A, time_increment, "Acceleration Graph", "Time", "Acceleration")
 
-    A_sum = []; A_sum.append(0)
-    V = []; V.append(0)
-    for i in range(1, len(A)):
-        A_sum.append(A[i] + A_sum[i-1])
-        V.append(A_sum[i])
+    # Adding the values of a0^+ and a0^-
+    x = [0, 0]
+    for i in range(2*n):
+        x.append(sol[i])
 
-    plotDiscretization(V, time_increment, "Velocity Graph", "Time", "Velocity")
+    if is_single:
+        A = find_accelerations(x, 2*n + 2)
+        print("Accelerations:")
+        print(A)
 
-    V_sum = []; V_sum.append(0)
-    POS = []; POS.append(0)
-    for i in range(1, len(V)):
-        V_sum.append(V[i] + V_sum[i-1])
-        POS.append(V_sum[i])
-        plotDiscretization(POS, time_increment, "Position Graph", "Time", "Position")
+        plot_discretization(A, time_increment, "Acceleration Graph", "Time", "Acceleration")
 
-    '''
+        A_sum = []; A_sum.append(0)
+        V = []; V.append(0)
+        for i in range(1, len(A)):
+            A_sum.append(A[i] + A_sum[i-1])
+            V.append(A_sum[i])
+
+        plot_discretization(V, time_increment, "Velocity Graph", "Time", "Velocity")
+
+        V_sum = []; V_sum.append(0)
+        POS = []; POS.append(0)
+        for i in range(1, len(V)):
+            V_sum.append(V[i] + V_sum[i-1])
+            POS.append(V_sum[i])
+        plot_discretization(POS, time_increment, "Position Graph", "Time", "Position")
+
+    return get_cost(c_copy, sol)
+
+def main():
+    opt = int(input("Enter 1 to choose to run a single problem. Enter 2 to run a test with several discretization intervals or type 3 to exit: "))
+
+    while (opt == 1 or opt == 2):
+        if (opt == 1):
+            time_increment = float(input('Enter the discretization interval: '))
+            problem(time_increment, True)
+        else:
+            x = []
+            y = []
+            x.append(2)
+            for i in range(1, 10):
+                x.append(x[i-1]/2)
+            for i in range(len(x)):
+                y.append(problem(x[i], False))
+            fig, ax = plt.subplots(1)
+            plt.ylim([0,1.1])
+            plt.xlabel("Discretization interval")
+            plt.ylabel("Final cost")
+            plt.title("(Fuel) Cost x Discretization interval")
+            plt.plot(x, y, 'ro')
+            plt.show()
+        opt = int(input("Enter 1 to choose to run a single problem. Enter 2 to run a test with several discretization intervals or type 3 to exit: "))
+
 main()
